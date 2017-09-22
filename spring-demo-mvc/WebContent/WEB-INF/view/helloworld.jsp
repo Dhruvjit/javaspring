16a3297442a52366824e2d522d83e79d5618409b
Dhruvjit <idhruvjit@gmail.com>
2017-09-22 07:22:55 +0200

    completed reading html form - write some code part 2 from home laptop

---
+++ spring-demo-mvc/WebContent/WEB-INF/view/helloworld.jsp
@@ -0,0 +1,11 @@
+<html>
+	<body>
+	
+		Hell world of Spring!
+		
+		<br></br>
+		
+		Student name: ${param.studentName}
+		
+	</body>
+</html>	
--- spring-demo-mvc/src/springdemo/mvc/HelloWorldController.java
+++ spring-demo-mvc/src/springdemo/mvc/HelloWorldController.java
@@ -9,10 +9,15 @@ public class HelloWorldController {
 	// need a controller method to show the initial HTML form
 	@RequestMapping("/showForm")
 	public String showForm(){
+		
+		// this will be passed to helloworld-jsp name 
 		return "helloworld-form";
 	}
 	
 	// need a controller method to process the HTML form
-	//
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "helloworld";
+	}
 	
 }
